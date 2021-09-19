bda50b4e6628f52bb539431b558524f7cba19411
David <zein207@gmail.com>
2021-09-19 14:36:04 -0500

    Add user logout

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,11 +1,22 @@
 import React, { useContext } from 'react'
-import { Link, NavLink } from 'react-router-dom'
+import { Link, NavLink, useHistory } from 'react-router-dom'
 import { AuthContext } from '../../auth/AuthContext';
+import { types } from '../../types/types';
 
 
 export const Navbar = () => {
 
-    const { user } = useContext( AuthContext );
+    const { user, dispatch } = useContext( AuthContext );
+    const history = useHistory();
+
+    const handleLogout = () => {
+
+        history.replace('/login');
+
+        dispatch({
+            type: types.logout
+        })
+    }
 
     return (
         <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
@@ -57,14 +68,12 @@ export const Navbar = () => {
                             user.name
                         }
                     </span>
-                    <NavLink 
-                        activeClassName="active"
-                        className="nav-item nav-link" 
-                        exact
-                        to="/login"
+                    <button 
+                        className="nav-item nav-link btn"
+                        onClick={ handleLogout }
                     >
                         Logout
-                    </NavLink>
+                    </button>
                 </ul>
             </div>
         </nav>
