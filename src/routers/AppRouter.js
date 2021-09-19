8cfd6cc830b42d7dbc8f2ad09742b35bb42bb38f
David <zein207@gmail.com>
2021-09-19 15:14:00 -0500

    Add public route

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,12 +1,12 @@
 import React, { useContext } from 'react'
 import {
   BrowserRouter as Router,
-  Switch,
-  Route
+  Switch
 } from "react-router-dom";
 
 import { AuthContext } from '../auth/AuthContext';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 import { LoginScreen } from '../components/login/LoginScreen';
 import { DashboardRoutes } from './DashboardRoutes';
@@ -19,12 +19,17 @@ export const AppRouter = () => {
         <Router>
             <div>
                 <Switch>
-                    <Route exact path="/login" component={ LoginScreen } />
+                    <PublicRoute 
+                        exact 
+                        path="/login" 
+                        component={ LoginScreen }
+                        isAuthenticated={ user.logged }
+                    />
 
                     <PrivateRoute 
                         path="/" 
                         component={ DashboardRoutes }
-                        isAuthenticated={ user.logged } 
+                        isAuthenticated={ user.logged }
                     />
                 </Switch>
             </div>
