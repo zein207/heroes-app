c7b44b0933be25304d030551ee10697c25218817
David <zein207@gmail.com>
2021-09-26 16:24:33 -0500

    Test in Navbar

---
+++ src/test/components/Navbar.test.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { mount } from "enzyme";
+import '@testing-library/jest-dom';
+
+import { AuthContext } from '../../auth/AuthContext';
+import { Navbar } from '../../components/ui/Navbar';
+import { MemoryRouter, Router } from 'react-router';
+import { types } from '../../types/types';
+
+describe('Test in <Navbar />', () => {
+
+    const historyMock = {
+        push: jest.fn(),
+        replace: jest.fn(),
+        location: {},
+        listen: jest.fn(),
+        createHref: jest.fn()
+    };
+
+    const contextValue = {
+        dispatch: jest.fn(),
+        user: {
+            logged: false,
+            name: 'Dula Peep'
+        }
+    };
+
+    const wrapper = mount(
+        <AuthContext.Provider value={ contextValue }>
+            <MemoryRouter >
+                <Router history={ historyMock }>
+                    <Navbar />
+                </Router>
+            </MemoryRouter>
+        </AuthContext.Provider>
+    );
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test('should show the component correctly', () => {
+
+        expect( wrapper ).toMatchSnapshot();
+        expect( wrapper.find('.text-info').text().trim() ).toBe('Dula Peep');
+        
+    });
+
+    test('should call logout function and use history', () => {
+        
+        wrapper.find('button').prop('onClick')();
+
+        expect( contextValue.dispatch ).toHaveBeenCalledWith({
+            type: types.logout
+        });
+
+        expect( historyMock.replace ).toHaveBeenCalledWith('/login');
+
+    })
+    
+    
+    
+})
